38bdaf4fc58a5a43e5e8dd96ccc59f25fa759d29
Victor Leão <110788743+victorleaoQA@users.noreply.github.com>
2023-07-18 23:34:06 -0300

    wip: toogle

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // toggle icon navbar 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+}
 
 
 // scroll section 
